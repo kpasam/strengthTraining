4086514c6d89b241ed276d32119ad382394175ad
Kartheek Pasam <kartheekpasam@gmail.com>
2026-03-28 14:06:06 -0700

    add android app support

---
+++ android-app/app/build/generated/source/buildConfig/androidTest/debug/com/strengthtraining/mobile/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.strengthtraining.mobile.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.strengthtraining.mobile.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String BASE_URL = "https://gym-tracker-kpasam.fly.dev";
+}
---
+++ android-app/app/proguard-rules.pro
@@ -0,0 +1,1 @@
+# Project-specific ProGuard rules can go here if a release build needs to shrink later.
